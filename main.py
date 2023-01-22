2b9636839514108c75c57d24acdec03076304b84
night3098 <122676310+night3098@users.noreply.github.com>
2023-01-22 15:32:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests, bs4, re
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Title", "URL"]
+
+def gethub(url, cssclass, firsturl = 'https://habr.com'):
+    habr_urls = {}
+    s = requests.get(url)
+    b = bs4.BeautifulSoup(s.text, "html.parser")
+    p = b.select(cssclass)
+    for x in p:
+        url = firsturl + x.get('href')
+        title = x.span.getText().strip()
+        p = '[^0-9a-zA-Zа-яА-Я .:,!-—()_ ЁёьЬъЪ\n\r\t]'
+        reg = re.compile(p)
+        title=reg.sub('', title)
+        habr_urls[url] = title
+        table.add_row([title, url])
+    return table
+    #return title, url
+    #return habr_urls
+
+url = 'https://habr.com/ru/hub/python/'
+cssclass = '.tm-article-snippet__title-link'
+print(gethub(url, cssclass))
